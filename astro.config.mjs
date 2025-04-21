654bf480b303d4f834f0f12f74f09c50e2c22ebc
Nick K <nick.k@trieve.ai>
2025-04-21 02:31:39 -0700

    feat: restructure project configuration and add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import { defineConfig } from "astro/config";
 import markdoc from "@astrojs/markdoc";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
-
 import icon from "astro-icon";
-
 import mdx from "@astrojs/mdx";
-import { SiteUrl } from "theme.config";
+import { SiteUrl } from "./src/theme.config";
 
 // https://astro.build/config
 export default defineConfig({
